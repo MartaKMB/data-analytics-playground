5984b2fa0d43ccf3f6e87d699cfc92a17ff837bb
MartaKMB <muszkamarta@interia.pl>
2026-04-08 09:14:13 +0200

    python: introduction

---
+++ data-heroes/python/podstawy/typy_operatory.py
@@ -0,0 +1,20 @@
+# 8.2.1
+print("Hello World")
+
+# 8.2.2
+# BMI = weight / height^2
+weight = 70
+height = 1.75
+result = weight / (height**2)
+
+print(result)
+
+a = "21"
+int(a)
+print(a + "str", int(a) + int(a), type(a))
+
+weight_input = input("weight [kg]: ")
+height_input = input(" height [m]: ")
+BMI = float(weight_input) / (float(height)**2)
+
+print(BMI)
